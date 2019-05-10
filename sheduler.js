b493ed8b3a5101b6761134cae907eec2047b37e2
Damika Gamlath <damika97.10@gmail.com>
2019-05-10 17:32:43 +0530

    Process add and remove

--- sheduler.js
+++ sheduler.js
@@ -12,7 +12,8 @@ function add(){
         arrival:$("#Addarrival").val(),
         pColour:$("#pColour").val(),
         runtime:0,
-        total:0
+        total:0,
+        availability:true,
     };
 
     var item=pQueueBig[pQueueBig.length-1];
@@ -20,8 +21,9 @@ function add(){
     var row2="<td>"+item.burst+"</td>";
     var row3="<td>"+item.arrival+"</td>";
     var row4="<td><canvas style=\"background: "+item.pColour+";height:30px;width:60px; border: solid;\"></canvas></td>"
+    var row5="<td> <button class=\"btn btn-danger\" onclick='removeProcess("+(pQueueBig.length-1)+")'>Remove</button></td>";
 
-    $("#iProcessTbl").append("<tr id=\"trele\">"+row1+row2+row3+row4+"</tr>");
+    $("#iProcessTbl").append("<tr id=\"trele\">"+row1+row2+row3+row4+row5+"</tr>");
    // $("#td").text(item.name+" "+ item.burst+ " " + item.arrival);
 }
 
@@ -31,11 +33,16 @@ let shedule = async () => {
     await timeout(500);
     time=0;
 
-    var pQueue = JSON.parse(JSON.stringify(pQueueBig));
+    // var pQueue = JSON.parse(JSON.stringify(pQueueBig));
+    var pQueue=[];
+    for(var i=0;i<pQueueBig.length;i++){
+        if (pQueueBig[i].availability==false)continue;
+        pQueue[pQueue.length]=pQueueBig[i];
+    }
     pQueue.sort((a, b) => (a.arrival > b.arrival) ? 1 : -1);
 
-    console.log("started");
-    console.log(pQueue);
+    // console.log("started");
+    // console.log(pQueue);
 
     while(pQueue.length>0 || readyQueue.length>0){
 
@@ -63,7 +70,7 @@ let shedule = async () => {
             readyQueue[0].runtime+=1;
             readyQueue[0].total+=1;
             addItem(readyQueue[0].pColour);
-            console.log("running"+readyQueue[0].name)
+            // console.log("running"+readyQueue[0].name)
         }
         else{
             addItem("white");
@@ -91,9 +98,9 @@ function addItem(clr) {
 
     $("#_sim").append("<div>\n" +
         "            <canvas style=\"display:block;background:"+clr+";height:75px;width:50px;position:relative;border:solid;\"></canvas>\n" +
+
         "<h2 style=\"display:block;background:red;height:50px;width:50px;position:relative;border:solid; text-align: center;\">"+time+"</h2>"+
         "        </div>");
-
 
 }
 
@@ -105,10 +112,40 @@ function reset(){
         "            <th>Burst Time</th>\n" +
         "            <th>Arrival Time</th>\n" +
         "            <th>Colour</th>\n" +
+        "            <th></th>\n" +
         "        </tr>");
     $("#_sim").empty();
 }
 
+function removeProcess(procesid){
+    $("#iProcessTbl").empty();
+    $("#iProcessTbl").append("<tr>\n" +
+        "            <th>Process Name</th>\n" +
+        "            <th>Burst Time</th>\n" +
+        "            <th>Arrival Time</th>\n" +
+        "            <th>Colour</th>\n" +
+        "            <th></th>\n" +
+        "        </tr>");
+
+    pQueueBig[procesid].availability=false;
+
+    var i=0;
+    for(i=0;i<pQueueBig.length;i++){
+        var item=pQueueBig[i];
+
+        if(item.availability==false)continue;
+
+        var row1="<td>"+item.name+"</td>";
+        var row2="<td>"+item.burst+"</td>";
+        var row3="<td>"+item.arrival+"</td>";
+        var row4="<td><canvas style=\"background: "+item.pColour+";height:30px;width:60px; border: solid;\"></canvas></td>"
+        var row5="<td> <button class=\"btn btn-danger\" onclick='removeProcess("+(i)+")'>Remove</button></td>";
+
+        $("#iProcessTbl").append("<tr id=\"trele\">"+row1+row2+row3+row4+row5+"</tr>");
+    }
+
+
+}
 $(function () {
     $('#cp1')
         .colorpicker({
